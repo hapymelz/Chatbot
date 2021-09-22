08836f73fd1e31a27868e973321c8c7965e71368
hapymelz <89086928+hapymelz@users.noreply.github.com>
2021-09-22 08:21:52 -0600

    added chatbot model.

---
+++ src/chat/model/Chatbot.java
@@ -0,0 +1,26 @@
+package chat.model;
+
+public class Chatbot
+{
+	private String name;
+	
+	
+	public Chatbot()
+	{
+		this.name = "Chatbot";
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	
+	
+}
+	
